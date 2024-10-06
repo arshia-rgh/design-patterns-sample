7fb77823e5f20c6b43e8de14289b6dfb2060df66
arshia <arshiarezagholi1212@gmail.com>
2024-10-06 06:07:49 +0330

    added factory method for the golang

--- golang/creational/factory_method.go
+++ golang/creational/factory_method.go
@@ -1,1 +1,67 @@
 package creational
+
+import (
+	"errors"
+)
+
+type ICar interface {
+	setName(name string)
+	getName() string
+	setSpeed(speed int)
+	getSpeed() int
+}
+
+type Car struct {
+	name  string
+	speed int
+}
+
+func (car *Car) setName(name string) { car.name = name }
+func (car *Car) getName() string     { return car.name }
+func (car *Car) setSpeed(speed int)  { car.speed = speed }
+func (car *Car) getSpeed() int       { return car.speed }
+
+type BMW struct {
+	Car
+}
+
+func newBMW() ICar {
+	return &BMW{Car{
+		name:  "BMW",
+		speed: 300,
+	}}
+}
+
+type Benz struct {
+	Car
+}
+
+func newBenz() ICar {
+	return &Benz{Car{
+		name:  "Benz",
+		speed: 250,
+	}}
+}
+
+// factory
+
+func getCar(carType string) (ICar, error) {
+	if carType == "BMW" {
+		return newBMW(), nil
+	}
+
+	if carType == "Benz" {
+		return newBenz(), nil
+	}
+
+	return nil, errors.New("wrong car type")
+}
+
+//func main() {
+//	bmw, _ := getCar("BMW")
+//	benz, _ := getCar("Benz")
+//
+//	fmt.Println(bmw.getName(), bmw.getSpeed())
+//	fmt.Println(benz.getName(), benz.getSpeed())
+//
+//}
