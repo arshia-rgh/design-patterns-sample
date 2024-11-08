8ae0a52c08b0b18774047a02b35504d8e240cf99
arshia <arshiarezagholi1212@gmail.com>
2024-11-08 12:06:21 +0330

    added the Journal struct and addentry method

--- golang/solid/single_responsibility.go
+++ golang/solid/single_responsibility.go
@@ -1,1 +1,15 @@
 package solid
+
+import "fmt"
+
+type Journal struct {
+	Entries []string
+}
+
+var entryCount = 0
+
+func (j *Journal) AddEntry(entry string) int {
+	entryCount++
+	j.Entries = append(j.Entries, fmt.Sprintf("%v: %v", entryCount, entry))
+	return entryCount
+}
