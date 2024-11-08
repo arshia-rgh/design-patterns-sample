b9de50274a347422bcf98ca9bfd5c5fba1791f96
arshia <arshiarezagholi1212@gmail.com>
2024-11-08 12:09:32 +0330

    added the RemoveENtry method for Journal

--- golang/solid/single_responsibility.go
+++ golang/solid/single_responsibility.go
@@ -13,3 +13,8 @@ func (j *Journal) AddEntry(entry string) int {
 	j.Entries = append(j.Entries, fmt.Sprintf("%v: %v", entryCount, entry))
 	return entryCount
 }
+
+func (j *Journal) RemoveEntry(index int) {
+	entryCount--
+	j.Entries = append(j.Entries[:index], j.Entries[index+1:]...)
+}
