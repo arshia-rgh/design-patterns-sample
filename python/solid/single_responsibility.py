59640cf401fa51540940c99ca2e516cd5a361e0c
arshia <arshiarezagholi1212@gmail.com>
2024-11-08 14:17:10 +0330

    added the class persistence

---
+++ python/solid/single_responsibility.py
@@ -0,0 +1,30 @@
+class Journal:
+    entries = []
+    count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f'{self.count}: {text}')
+        self.count += 1
+
+    def remove_entry(self, index):
+        self.entries.pop(index)
+        self.count -= 1
+
+    # the persistence methods break the single responsibility
+    def save_to_file(self, file_name):
+        pass
+
+    def load_from_file(self, file_name):
+        pass
+
+
+# should separate the persistence (another class or just functions)
+class Persistence:
+    def __init__(self, line_separator):
+        self.line_separator = line_separator
+
+    def save_to_file(self, file_name):
+        pass
+
+    def load_from_file(self, file_name):
+        pass
