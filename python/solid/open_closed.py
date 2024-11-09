3312505df6e9997b9b58ec8a489fc0b934419362
arshia <arshiarezagholi1212@gmail.com>
2024-11-09 21:21:35 +0330

    added comments for the FIlter

--- python/solid/open_closed.py
+++ python/solid/open_closed.py
@@ -4,5 +4,19 @@ class Product:
         self.size = size
 
 
+# In Filter class if we need to add another filters like (filter_by_color_and_size)
+# we need to modify the Filter class and add new methods, and this will break the OCP
 class Filter:
-    pass
+    def filter_by_color(self, color, products):
+        result = []
+        for i in products:
+            if i.color == color:
+                result.append(i)
+        return result
+
+    def filter_by_size(self, size, products):
+        result = []
+        for i in products:
+            if i.size == size:
+                result.append(i)
+        return result
