1aefb88feac8e88e954a0d52962d97b426c27ec5
arshia <arshiarezagholi1212@gmail.com>
2024-11-09 18:52:52 +0330

    added the BetterFilter struct and this will use the Specification iterface and doesnt break the OCP rule

--- golang/solid/open_closed.go
+++ golang/solid/open_closed.go
@@ -30,9 +30,26 @@ type Product struct {
 // Filter actually breaks the OCP because it will be modify over and over
 type Filter struct{}
 
-func (f *Filter) FilterByColor(products []Product, color Color) []*Product {}
+func (f *Filter) FilterByColor(products []Product, color Color) []*Product {
+	result := make([]*Product, 0)
+	for i, v := range products {
+		if v.color == color {
+			result = append(result, &products[i])
+		}
+	}
+	return result
+}
 
-func (f *Filter) FilterBySize(products []Product, size Size) []*Product {}
+func (f *Filter) FilterBySize(products []Product, size Size) []*Product {
+	result := make([]*Product, 0)
+	for i, v := range products {
+		if v.size == size {
+			result = append(result, &products[i])
+		}
+	}
+	return result
+}
+}
 
 // More ... ( we need modify the Filter type) --> breaks the OCP
 
@@ -75,4 +92,20 @@ type AndSpecification struct {
 
 func (as *AndSpecification) IsSatisfied(p *Product) bool {
 	return as.first.IsSatisfied(p) && as.second.IsSatisfied(p)
+}
+
+/*
+	This filter type now doesnt break the OCP
+*/
+
+type BetterFilter struct{}
+
+func (bf *BetterFilter) Filter(products []Product, spec Specification) []*Product {
+	result := make([]*Product, 0)
+	for i, v := range products {
+		if spec.IsSatisfied(&v) {
+			result = append(result, &products[i])
+		}
+	}
+	return result
 }
