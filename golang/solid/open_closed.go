09a897c9dc0ba47d017b72f8e42c32ebd01ac554
arshia <arshiarezagholi1212@gmail.com>
2024-11-09 18:36:08 +0330

    added the specification interface

--- golang/solid/open_closed.go
+++ golang/solid/open_closed.go
@@ -23,10 +23,11 @@ type Product struct {
 }
 
 /*
-Here if we want to add new filter or combine of the filters we need to repeatedly modify the existing Filter struct
-and this will break the open-closed principle
+	Here if we want to add new filter or combine of the filters we need to repeatedly modify the existing Filter struct
+	and this will break the open-closed principle
 */
 
+// Filter actually breaks the OCP because it will be modify over and over
 type Filter struct{}
 
 func (f *Filter) FilterByColor(products []Product, color Color) []*Product {}
@@ -34,3 +35,23 @@ func (f *Filter) FilterByColor(products []Product, color Color) []*Product {}
 func (f *Filter) FilterBySize(products []Product, size Size) []*Product {}
 
 // More ... ( we need modify the Filter type) --> breaks the OCP
+
+// -- (Solution) --
+// We can use `Specification pattern`
+
+type Specification interface {
+	IsSatisfied(p *Product) bool
+}
+
+/*
+	We can implement types for each Filter we want to add and
+	made them all implement the same interface
+*/
+
+type ColorSpecification struct {
+	color Color
+}
+
+func (cs *ColorSpecification) IsSatisfied(p *Product) bool {
+	return p.color == cs.color
+}
