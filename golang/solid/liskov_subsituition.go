2fc125ddcf661b44aaece38ce4392549483c5f52
arshia <arshiarezagholi1212@gmail.com>
2024-11-14 23:07:46 +0330

    added the UseIt function

--- golang/solid/liskov_subsituition.go
+++ golang/solid/liskov_subsituition.go
@@ -1,5 +1,7 @@
 package solid
 
+import "fmt"
+
 type Sized interface {
 	GetWidth() int
 	SetWidth(width int)
@@ -15,3 +17,16 @@ func (r *Rectangle) GetWidth() int        { return r.width }
 func (r *Rectangle) SetWidth(width int)   { r.width = width }
 func (r *Rectangle) GetHeight() int       { return r.height }
 func (r *Rectangle) SetHeight(height int) { r.height = height }
+
+type Square struct {
+	Rectangle
+}
+
+func UseIt(sized Sized) {
+	width := sized.GetWidth()
+	sized.SetHeight(10)
+	expectedArea := 10 * width
+	actualArea := sized.GetHeight() * sized.GetWidth()
+
+	fmt.Println("expected: ", expectedArea, "actual: ", actualArea)
+}
