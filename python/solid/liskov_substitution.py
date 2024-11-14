0d9562d0907b81f21c1986d99f2e3b480aec6308
arshia <arshiarezagholi1212@gmail.com>
2024-11-14 23:29:33 +0330

    added the Rectangle class

---
+++ python/solid/liskov_substitution.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def get_width(self):
+        pass
+
+    def set_width(self, width):
+        pass
+
+    def get_height(self):
+        pass
+
+    def set_height(self, height):
+        pass
